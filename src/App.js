8c9524d2c99469acc92c15ba02ff87b7ca1673bd
Meet panchal <meetpanchal1707@gmail.com>
2020-02-28 11:31:40 +0530

    Basic of react-redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Counter: {this.props.ctr}</h3>
+        <button onClick={this.props.onIncCounter}>Increment</button>
+      </div>
+    );
+  }
 }
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncCounter: () => dispatch({ type: "INC_COUNTER" })
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
